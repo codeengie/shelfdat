f422d3b10f8ac5c2c8acfe7cc782898a6a7dabff
Cesar Villanueva Jr <cvillanujr@gmail.com>
2020-12-26 22:13:23 -0800

    Media Form and Errors
    Tweaked the media form to show errors for all the fields and added a
    few more styles to make it somewhat pretty.

--- src/components/NewMedia.vue
+++ src/components/NewMedia.vue
@@ -3,9 +3,9 @@
     <h3 class="media-form__title">Create A New Movie</h3>
 
     <div class="media-form__errors" v-if="errors.length">
-      <h4>Please correct the following error(s):</h4>
-      <ul>
-        <li v-for="error in errors" :key="error">{{ error }}</li>
+      <h4 class="media-form__errors-title">Please correct the following error(s):</h4>
+      <ul class="media-form__errors-list">
+        <li class="media-form__errors-item" v-for="error in errors" :key="error">{{ error }}</li>
       </ul>
     </div>
 
@@ -18,7 +18,7 @@
       <div class="media-form__group media-form__group--dropdown">
         <label class="media-form__label" for="format">Format</label>
         <select class="media-form__select" name="format" id="format">
-          <option value="null">Format</option>
+          <option value="null">Choose option</option>
           <option value="DVD">DVD</option>
           <option value="Blu-ray">Blu-ray</option>
           <option value="4K Blu-ray">4K Blu-ray</option>
@@ -28,7 +28,7 @@
       <div class="media-form__group media-form__group--dropdown">
         <label class="media-form__label" for="type">Type</label>
         <select class="media-form__select" name="type" id="type">
-          <option value="null">Type</option>
+          <option value="null">Choose option</option>
           <option value="Movie">Movie</option>
           <option value="TV Show">TV Show</option>
         </select>
@@ -37,7 +37,7 @@
       <div class="media-form__group media-form__group--dropdown">
         <label class="media-form__label" for="location">Location</label>
         <select class="media-form__select" name="location" id="location">
-          <option value="null">Location</option>
+          <option value="null">Choose option</option>
           <option value="1st Floor">1st Floor</option>
           <option value="2nd Floor">2nd Floor</option>
         </select>
@@ -46,7 +46,7 @@
       <div class="media-form__group media-form__group--dropdown">
         <label class="media-form__label" for="container">Container</label>
         <select class="media-form__select" name="container" id="container">
-          <option value="null">Container</option>
+          <option value="null">Choose option</option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -65,7 +65,11 @@
     data() {
       return {
         errors: [],
-        title: null
+        container: null,
+        format: null,
+        location: null,
+        title: null,
+        type: null
       };
     },
     methods: {
@@ -74,6 +78,22 @@
 
         if (!this.title) {
           this.errors.push('Title is required');
+        }
+
+        if (!this.format) {
+          this.errors.push('Format is required');
+        }
+
+        if (!this.type) {
+          this.errors.push('Type is required');
+        }
+
+        if (!this.location) {
+          this.errors.push('Location is required');
+        }
+
+        if (!this.container) {
+          this.errors.push('Container is required');
         }
 
         if (!this.errors.length) {
@@ -100,11 +120,28 @@
 
   .media-form__errors {
     color: #ff0000;
+    font-size: 1.2rem;
+  }
+
+  .media-form__errors-list {
+    margin-bottom: 0;
+    margin-top: 4px;
+    padding-left: 12px;
+  }
+
+  .media-form__errors-title {
+    margin-bottom: 0;
+    margin-top: 10px;
+  }
+
+  .media-form__group:not(:first-child) {
+    margin-top: 10px;
   }
 
   .media-form__label {
     color: #bebebe;
     display: inline-block;
+    font-size: 1.4rem;
     width: 20%;
   }
 
@@ -117,6 +154,11 @@
     color: #eee;
     font-size: 1.8rem;
     margin: 0;
+    text-align: center;
+  }
+
+  /* Modifiers */
+  .media-form__group--button {
     text-align: center;
   }
 </style>
