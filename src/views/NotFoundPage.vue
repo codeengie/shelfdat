ef4a06ec5217462b9947ebc8f9bce5c667349e0d
Cesar Villanueva Jr <cvillanujr@gmail.com>
2021-08-17 19:25:33 -0700

    404 Page
    Began carving out the 404 page, currently there is no design for this
    but I wanted to lay a bit of groundwork.

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
-        <h1>
-            <span>404 Page Not Found</span>
-            <span>There was a glitch in the Matrix!</span>
+    <div class="not-found">
+        <h1 class="not-found__header">
+            <span class="not-found__header-main">404 Page Not Found</span>
+            <span class="not-found__header-subtitle">There was a glitch in the Matrix!</span>
         </h1>
-        <p>We could&rsquo;t find the page you are looking, but don't fret sometimes getting lost is a good thing.</p>
+        <p class="not-found__text">We could&rsquo;t find the page you are looking, but don't fret sometimes getting lost is a good thing.</p>
     </div>
 </template>
 
@@ -14,6 +14,23 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.not-found {
+    text-align: center;
 
+    &__header {
+        &-main,
+        &-subtitle {
+            display: block;
+        }
+
+        &-main {
+            font-size: 2.6rem;
+        }
+
+        &-subtitle {
+            font-size: 1.8rem;
+        }
+    }
+}
 </style>
