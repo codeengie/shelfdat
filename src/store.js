b8e8037b5d1ae5b1e9ee5a2735ca35cfb2c51827
Cesar Villanueva Jr <cvillanujr@gmail.com>
2022-09-08 15:19:28 -0700

    Store
    Created basic mutations, actions and getters. At the moment they are
    not being used in the app. I'll be creating a new branch in Git so I
    can properly integrate the store.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,44 @@
 import { createStore } from 'vuex';
 
-// There can only be a single store for an application but you can have multiple states
+// There can only be a single store for an application, but you can have multiple states
 const store = createStore({
     state() {
         return {
+            collections: [],
             foobar: 0
+        }
+    },
+    // The only way to change state in a store
+    mutations: {
+        foobar(state) {
+            state.foobar++;
+        }
+    },
+    /**
+     * Actions are similar to mutations except instead of mutating state, actions commit
+     * mutations and can contain arbitrary asynchronous operations.
+     */
+    actions: {
+        // You have the same name
+        foobar(context) {
+            context.commit('foobar'); // Action commits foobar
+        }
+    },
+    // Think of them as computed properties for stores
+    getters: {
+        finalFoobar(state) {
+            return state.foobar * 2;
+        },
+        normalizedFoobar(state) {
+            const finalFoobar = state.counter * 3;
+
+            if (finalFoobar < 0) {
+                return 0;
+            }
+        },
+        // @todo Consolidate all the inventory counters you have in components
+        inventoryCount(state) {
+            return state;
         }
     }
 });
