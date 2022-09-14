9b229c7a5c1a46d77576dcdae154b12abfc8ddb3
Cesar Villanueva Jr <cvillanujr@gmail.com>
2022-09-13 20:42:37 -0700

    Line Loader
    Created a line loader component to display across the entire webpage.
    I'll come back later to tweak the animation settings.

---
+++ src/components/LineLoader.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="line-loader"></div>
+</template>
+
+<script>
+export default {
+    name: 'LineLoader'
+}
+</script>
+
+<style lang="scss" scoped>
+$loader-height: 3px;
+
+.line-loader {
+    background-color: #ddd;
+    height: $loader-height;
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+
+    &::before {
+        animation: lineAnime 1s linear infinite;
+        background-color: coral;
+        content: '';
+        height: $loader-height;
+        left: -50%;
+        position: absolute;
+        width: 40%;
+    }
+
+    @keyframes lineAnime {
+        0% {
+            left: -40%;
+        }
+        50% {
+            left: 20%;
+            width: 80%;
+        }
+        100% {
+            left: 100%;
+            width: 100%
+        }
+    }
+}
+</style>
