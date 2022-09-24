b1640d8108e53940af3d1f041b04d622974b8134
Cesar Villanueva Jr <cvillanujr@gmail.com>
2022-09-24 12:20:56 -0700

    Dashboard Cleanup
    Removed all commented code from previous iteration and took care of the
    todos regarding using loops for ChartStat component. I did create
    segment and type data but I will consolidate this in the future.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -17,75 +17,26 @@
             </div>
 
             <!-- Donut legend with counts -->
-            <!-- @todo Use a loop -->
             <!-- @todo Change class name to 'legend' and rename ChatStat component -->
             <div class="segments">
                 <ChartStat
+                    v-for="(segment, index) in donutSegmentData"
+                    :key="segment"
                     :stat-mods="['icon']"
-                    :stat-num="donutSegmentData[0]"
-                    stat-icon-color="#007aaf"
-                    stat-label="4K"/>
-                <ChartStat
-                    :stat-mods="['icon']"
-                    :stat-num="donutSegmentData[1]"
-                    stat-icon-color="#a24a59"
-                    stat-label="BRAY"/>
-                <ChartStat
-                    :stat-mods="['icon']"
-                    :stat-num="donutSegmentData[2]"
-                    stat-icon-color="#a27c4a"
-                    stat-label="DVD"/>
+                    :stat-num="donutSegmentData[index]"
+                    :stat-icon-color="donutData.datasets[0].backgroundColor[index]"
+                    :stat-label="donutData.labels[index]"/>
             </div>
 
-            <!-- Total inventory count by category -->
-            <!-- @todo Use a loop -->
+            <!-- Total inventory by category -->
             <div class="category">
                 <ChartStat
-                    :stat-num="donutLegendData[0]"
-                    stat-label="Movies"/>
-                <ChartStat
-                    :stat-num="donutLegendData[1]"
-                    stat-label="TV Shows"/>
+                    v-for="(legend, index) in donutLegendData"
+                    :key="legend"
+                    :stat-num="donutLegendData[index]"
+                    :stat-label="donutTypes[index]"/>
             </div>
         </div>
-
-        <!-- Media Section -->
-        <!--<nas-modwrap :modifier="['foobar', 'bg']">
-            <template #heading>Media</template>
-            <template #default>
-                <div class="doughnut">
-                    <div class="stat stat--flip">
-                        <h3 class="stat__title">Total</h3>
-                        <h4 class="stat__reading">{{ inventoryTotal }}</h4>
-                    </div>
-                </div>
-                <div class="media-stats">
-                    <Stat
-                        v-for="stat in stats"
-                        :key="stat"
-                        :db-data="inventoryData"
-                        :filter-by="stat.filterBy"
-                        :filter-key="stat.filterKey"
-                        :modifier="stat.modifier"
-                        :title="stat.title"/>
-                </div>
-            </template>
-        </nas-modwrap>-->
-        <!--<nas-modwrap>
-            <template #default>
-                <div class="sub-stats">
-                    <Stat
-                        v-for="stat in subStats"
-                        :key="stat"
-                        :db-data="inventoryData"
-                        :iconName="stat.iconName"
-                        :filter-by="stat.filterBy"
-                        :filter-key="stat.filterKey"
-                        :modifier="stat.modifier"
-                        :title="stat.title"/>
-                </div>
-            </template>
-        </nas-modwrap>-->
         <!-- Display latest inventory items -->
         <!--<LatestItems
             :db-data="inventoryData"
@@ -99,7 +50,6 @@ import ChartStat from '../components/ChartStat';
 import DoughnutChart from '../components/DoughnutChart';
 //import LatestItems from '../components/LatestItems';
 import LineLoader from '../components/LineLoader';
-//import Stat from '../components/Stat';
 
 export default {
     name: 'Dashboard',
@@ -107,55 +57,20 @@ export default {
         ChartStat,
         DoughnutChart,
         //LatestItems,
-        LineLoader//,
-        //Stat
+        LineLoader
     },
     data() {
         return {
+            // Consolidate all this donut data while keeping in mind it doesn't break
             donutName: 'pinky-w-sprinkles',
             donutData: {
-                labels: ['4K Blu-ray', 'Blu-ray', 'DVD'],
+                labels: ['4K', 'Blu-ray', 'DVD'],
                 datasets: [{
                     backgroundColor: ['#007aaf', '#a27c4a', '#a24a59'],
                     data: []
                 }]
             },
-            stats: [
-                {
-                    filterKey: '4K Blu-ray',
-                    filterBy: 'format',
-                    modifier: 'picton',
-                    title: '4K'
-                },
-                {
-                    filterKey: 'Blu-ray',
-                    filterBy: 'format',
-                    modifier: 'golden',
-                    title: 'BRay'
-                },
-                {
-                    filterKey: 'DVD',
-                    filterBy: 'format',
-                    modifier: 'wild',
-                    title: 'DVD'
-                }
-            ],
-            subStats: [
-                {
-                    filterKey: 'Movie',
-                    filterBy: 'type',
-                    iconName: 'movie.svg',
-                    modifier: 'icon',
-                    title: 'Movies'
-                },
-                {
-                    filterKey: 'TV Show',
-                    filterBy: 'type',
-                    iconName: 'tv.svg',
-                    modifier: 'icon',
-                    title: 'TV Shows'
-                }
-            ]
+            donutTypes: ['Movies', 'TV Shows']
         };
     },
     created() {
@@ -180,19 +95,7 @@ export default {
         ]),
     },
     methods: {
-        ...mapActions(['getInventory']),
-        // @todo Remove, you refactored this code and combined it in the store
-        /*createDonutData(prop) {
-            this.donutData.labels.forEach(label => {
-                console.log(label);
-                this.donutData.datasets[0].data.push(this.inventoryCount(prop, label));
-            });
-        },
-        inventoryCount(filterBy, filterKey) {
-            let count = this.inventoryData.filter(item => item[filterBy] === filterKey).length;
-            console.log(count);
-            return count;
-        }*/
+        ...mapActions(['getInventory'])
     }
 }
 </script>
