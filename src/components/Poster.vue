8cb0a61a676f6eb4774167d07adb5d1e7a8a4f09
Cesar Villanueva Jr <cvillanujr@gmail.com>
2022-09-25 21:28:03 -0700

    Poster Component
    Created poster component, it will be used to display inventory items.
    I've temporarily set it to display the inventory items I was already
    looping through, but will make further changes as I build other
    components.

---
+++ src/components/Poster.vue
@@ -0,0 +1,51 @@
+<template>
+    <figure class="poster">
+        <a class="poster__link" href="#">
+            <img
+                :data-uid="posterId"
+                :height="posterHeight"
+                :src="srcImage"
+                :title="posterTitle"
+                :width="posterWidth"
+                @click="getId"
+                alt="Poster"
+                class="poster_img"
+                loading="lazy">
+        </a>
+    </figure>
+</template>
+
+<script>
+export default {
+    name: 'Poster',
+    props: {
+        posterHeight: {
+            default: '248',
+            type: String
+        },
+        posterId: String,
+        posterSrc: String,
+        posterTitle: String,
+        posterWidth: {
+            default: '166',
+            type: String
+        }
+    },
+    computed: {
+        srcImage() {
+            return this.posterSrc ? `${process.env.VUE_APP_S3_BUCKET_URL}images/inventory/posters/${this.posterSrc}` : 'images/placeholder.svg';
+        }
+    },
+    methods: {
+        getId(event) {
+            console.log(event);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.poster {
+    background-color: var(--shark);
+}
+</style>
