06cfcde7c35ca6c85843e3c19710fd713b756cac
Cesar Villanueva Jr <cvillanujr@gmail.com>
2022-10-30 21:41:11 -0700

    Jenkins Build Errors
    Issued a temporary fix to get Jenkins building and deploying. The issue
    is caused because the '.certs' folder is not in the project when it
    pulls the repo from GitLab. This is by design as that folder should be
    in the repo and is meant for local development. I mickey moused a fix
    but I left a todo to come back and issue a real fix.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-    devServer: {
-        host: 'shelfdat.com',
-        https: {
-            key: fs.readFileSync('.certs/shelfdat.com+5-key.pem'),
-            cert: fs.readFileSync('.certs/shelfdat.com+5.pem')
+const { defineConfig } = require('@vue/cli-service');
+
+// @todo Look for a better way to achieve this without repeating yourself
+if (process.env.NODE_ENV !== 'production') {
+    module.exports = defineConfig({
+        devServer: {
+            host: 'shelfdat.com',
+            https: {
+                key: fs.readFileSync('.certs/shelfdat.com+5-key.pem'),
+                cert: fs.readFileSync('.certs/shelfdat.com+5.pem')
+            },
+            port: 443
         },
-        port: 443
-    },
-    transpileDependencies: true
-})
+        transpileDependencies: true
+    });
+} else {
+    module.exports = defineConfig({
+        transpileDependencies: true
+    });
+}
