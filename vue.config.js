03cf3db701d026be673c35bbfb9b0f401eada1b1
Cesar Villanueva Jr <cvillanujr@gmail.com>
2023-01-27 21:17:00 -0800

    BrowserSync
    I installed BrowserSync in the project so I can test and debug the app
    but I wasn't able to debug remotely. I'm commenting the code for now so
    I can debug later.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { defineConfig } = require('@vue/cli-service');
+//const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 // @todo Look for a better way to achieve this without repeating yourself
 if (process.env.NODE_ENV !== 'production') {
@@ -12,6 +13,17 @@ if (process.env.NODE_ENV !== 'production') {
             },
             port: 443
         },
+        /*configureWebpack: {
+            plugins: [
+                new BrowserSyncPlugin({
+                    host: 'localhost',
+                    port: 3000,
+                    proxy: 'https://shelfdat.com'
+                },{
+                    reload: false
+                })
+            ],
+        },*/
         transpileDependencies: true
     });
 } else {
